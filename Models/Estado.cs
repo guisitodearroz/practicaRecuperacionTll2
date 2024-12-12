3422634262cec584b10f2a33acbd232a46d7fe43
Diego Cortez <dc936572@gmail.com>
2024-12-12 01:46:50 -0300

    primer commit

---
+++ Models/Estado.cs
@@ -0,0 +1,6 @@
+public enum Estado
+{
+    Pendiente,
+    EnProgreso,
+    Realizada
+}
---
+++ Models/Tarea.cs
@@ -0,0 +1,7 @@
+public class Tarea
+{
+    public int Id {get; set;}
+    public string Titulo {get; set;}
+    public string Descripcion {get;set;}
+    public Estado Estado {get; set;}
+}
